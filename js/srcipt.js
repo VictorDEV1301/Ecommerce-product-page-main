21b9fef4c60e7e6930596cf6d6f1aae0ecdd4118
João Victor <victor.dev1301@gmail.com>
2023-04-14 17:28:35 -0300

    função carrossel adicionada a telas desktop

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,7 +1,11 @@
 const menuMobile = document.querySelector('.menu');
 const cartPainel = document.querySelector('.cart');
-const imagemPrincipal = document.querySelector('#principal')
-const imagens = document.querySelectorAll('.alterar img')
+const imagemPrincipal = document.querySelector('#principal');
+const MimagemPrincipal = document.querySelector('#Mprincipal');
+const imagens = document.querySelectorAll('.alterar img');
+const mainTop = document.querySelector('.main-top');
+
+let imagem = 1;
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 700) {
@@ -22,12 +26,43 @@ document.addEventListener('click', e => {
     cartPainel.style.display = cartPainel.style.display === 'none' ? 'flex' : 'none';
   }
 
-  for (let imagem = 1; imagem < 6; imagem++){
+  for (let imagem = 1; imagem < 6; imagem++) {
     if (e.target.id === `img${imagem}`) {
       imagemPrincipal.setAttribute('src', `./assets/image-product-${imagem}.jpg`)
       imagens.forEach(valor => {
         valor.style.opacity = valor.id === e.target.id ? '0.75' : '1';
       })
+    }
+  }
+
+  if(window.innerWidth > 700){
+
+    for (let imagem = 1; imagem < 6; imagem++) {
+      if (e.target.id === `Mimg${imagem}`) MimagemPrincipal.setAttribute('src', `./assets/image-product-${imagem}.jpg`)
+    }
+
+    if (e.target.id === 'direita') {
+      imagem++;
+      if (imagem > 4) {
+        imagem = 4;
+      }
+      MimagemPrincipal.setAttribute('src', `./assets/image-product-${imagem}.jpg`)
+    }
+  
+    if (e.target.id === 'esquerda') {
+      imagem--;
+      if (imagem < 1) {
+        imagem = 1;
+      }
+      MimagemPrincipal.setAttribute('src', `./assets/image-product-${imagem}.jpg`)
+    }
+
+    if(e.target.id === 'principal'){
+      mainTop.style.display = 'flex';
+    }
+
+    if(e.target.id === 'closeImg'){
+      mainTop.style.display = 'none';
     }
   }
 
