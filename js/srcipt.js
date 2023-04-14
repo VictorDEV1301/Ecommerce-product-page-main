cafef28d167e235939bffc9523c99169fa364587
João Victor <victor.dev1301@gmail.com>
2023-04-14 15:15:47 -0300

    adicionado carrossel de imagens e parte da direita

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,5 +1,7 @@
 const menuMobile = document.querySelector('.menu');
 const cartPainel = document.querySelector('.cart');
+const imagemPrincipal = document.querySelector('#principal')
+const imagens = document.querySelectorAll('.alterar img')
 
 window.addEventListener('resize', () => {
   if (window.innerWidth > 700) {
@@ -19,4 +21,14 @@ document.addEventListener('click', e => {
   if (e.target.id === 'cart') {
     cartPainel.style.display = cartPainel.style.display === 'none' ? 'flex' : 'none';
   }
+
+  for (let imagem = 1; imagem < 6; imagem++){
+    if (e.target.id === `img${imagem}`) {
+      imagemPrincipal.setAttribute('src', `./assets/image-product-${imagem}.jpg`)
+      imagens.forEach(valor => {
+        valor.style.opacity = valor.id === e.target.id ? '0.75' : '1';
+      })
+    }
+  }
+
 })
